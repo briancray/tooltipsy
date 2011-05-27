13ab735741ea7c17215d7d441e58acb787029307
Brian Cray <bcrayzie@gmail.com>
2011-05-27 09:32:05 -0400

    Added delay option (set in milliseconds, defaults to 200)

--- tooltipsy.source.js
+++ tooltipsy.source.js
@@ -6,6 +6,7 @@
  *  - content: HTML or text content of tooltip. Defaults to "" (empty string), which pulls content from target element's title attribute
  *  - show: function(event, tooltip) to show the tooltip. Defaults to a show(100) effect
  *  - hide: function(event, tooltip) to hide the tooltip. Defaults to a fadeOut(100) effect
+ *  - delay: A delay in milliseconds before showing a tooltip. Set to 0 for no delay. Defaults to 200
  *  - css: object containing CSS properties and values. Defaults to {} to use stylesheet for styles
  *  - className: DOM class for styling tooltips with CSS. Defaults to "tooltipsy"
  * More information visit http://tooltipsy.com/
@@ -20,6 +21,7 @@
         this.shown = false;
         this.width = 0;
         this.height = 0;
+        this.delaytimer = null;
 
         this.$el.data("tooltipsy", this);
         this.init();
@@ -29,80 +31,103 @@
         var base = this;
 
         base.settings = $.extend({}, base.defaults, base.options);
+        base.settings.delay = parseInt(base.settings.delay);
 
         if (typeof base.settings.content === 'function') {
             base.readify(); 
         }
         
         base.$el.bind('mouseenter', function (e) {
-            if (base.ready === false) {
-                base.readify();
-            }
-            if (base.shown === false) {
-                if ((function (o) {
-                    var s = 0, k;
-                    for (k in o) {
-                        if (o.hasOwnProperty(k)) {
-                            s++;
-                        }
-                    }
-                    return s;
-                })(base.settings.css) > 0) {
-                    base.$tip.css(base.settings.css);
-                }
-                base.width = base.$tipsy.outerWidth();
-                base.height = base.$tipsy.outerHeight();
-            }
-
-            if (base.settings.alignTo == 'cursor') {
-                var tip_position = [e.pageX + base.settings.offset[0], e.pageY + base.settings.offset[1]];
-                if(tip_position[0] + base.width > $(window).width()) {
-                    var tip_css = {top: tip_position[1] + 'px', right: tip_position[0] + 'px', left: 'auto'};
-                }
-                else {
-                    var tip_css = {top: tip_position[1] + 'px', left: tip_position[0] + 'px', right: 'auto'};
-                }
+            if (base.settings.delay > 0) {
+                base.delaytimer = window.setTimeout(function () {
+                    base.enter(e);
+                }, base.settings.delay);
             }
             else {
-                var tip_position = [
-                    (function (pos) {
-                        if (base.settings.offset[0] < 0) {
-                            return pos.left - Math.abs(base.settings.offset[0]) - base.width;
-                        }
-                        else if (base.settings.offset[0] == 0) {
-                            return pos.left - ((base.width - base.$el.outerWidth()) / 2);
-                        }
-                        else {
-                            return pos.left + base.$el.outerWidth() + base.settings.offset[0];
-                        }
-                    })(base.offset(base.$el[0])),
-                    (function (pos) {
-                        if (base.settings.offset[1] < 0) {
-                            return pos.top - Math.abs(base.settings.offset[1]) - base.height;
-                        }
-                        else if (base.settings.offset[1] == 0) {
-                            return pos.top - ((base.height - base.$el.outerHeight()) / 2);
-                        }
-                        else {
-                            return pos.top + base.$el.outerHeight() + base.settings.offset[1];
-                        }
-                    })(base.offset(base.$el[0]))
-                ];
+                base.enter(e);
             }
-            base.$tipsy.css({top: tip_position[1] + 'px', left: tip_position[0] + 'px'});
-            base.settings.show(e, base.$tipsy.stop(true, true));
         }).bind('mouseleave', function (e) {
-            if (e.relatedTarget == base.$tip[0]) {
-                base.$tip.bind('mouseleave', function (e) {
-                    if (e.relatedTarget == base.$el[0]) {
-                        return;
-                    }
-                    base.settings.hide(e, base.$tipsy.stop(true, true));
-                });
-                return;
-            }
-            base.settings.hide(e, base.$tipsy.stop(true, true));
+            window.clearTimeout(base.delaytimer);
+            base.delaytimer = null;
+            base.leave(e);
         });
+    };
+
+    $.tooltipsy.prototype.enter = function (e) {
+        var base = this;
+
+        if (base.ready === false) {
+            base.readify();
+        }
+
+        if (base.shown === false) {
+            if ((function (o) {
+                var s = 0, k;
+                for (k in o) {
+                    if (o.hasOwnProperty(k)) {
+                        s++;
+                    }
+                }
+                return s;
+            })(base.settings.css) > 0) {
+                base.$tip.css(base.settings.css);
+            }
+            base.width = base.$tipsy.outerWidth();
+            base.height = base.$tipsy.outerHeight();
+        }
+
+        if (base.settings.alignTo == 'cursor') {
+            var tip_position = [e.pageX + base.settings.offset[0], e.pageY + base.settings.offset[1]];
+            if(tip_position[0] + base.width > $(window).width()) {
+                var tip_css = {top: tip_position[1] + 'px', right: tip_position[0] + 'px', left: 'auto'};
+            }
+            else {
+                var tip_css = {top: tip_position[1] + 'px', left: tip_position[0] + 'px', right: 'auto'};
+            }
+        }
+        else {
+            var tip_position = [
+                (function (pos) {
+                    if (base.settings.offset[0] < 0) {
+                        return pos.left - Math.abs(base.settings.offset[0]) - base.width;
+                    }
+                    else if (base.settings.offset[0] == 0) {
+                        return pos.left - ((base.width - base.$el.outerWidth()) / 2);
+                    }
+                    else {
+                        return pos.left + base.$el.outerWidth() + base.settings.offset[0];
+                    }
+                })(base.offset(base.$el[0])),
+                (function (pos) {
+                    if (base.settings.offset[1] < 0) {
+                        return pos.top - Math.abs(base.settings.offset[1]) - base.height;
+                    }
+                    else if (base.settings.offset[1] == 0) {
+                        return pos.top - ((base.height - base.$el.outerHeight()) / 2);
+                    }
+                    else {
+                        return pos.top + base.$el.outerHeight() + base.settings.offset[1];
+                    }
+                })(base.offset(base.$el[0]))
+            ];
+        }
+        base.$tipsy.css({top: tip_position[1] + 'px', left: tip_position[0] + 'px'});
+        base.settings.show(e, base.$tipsy.stop(true, true));
+    };
+
+    $.tooltipsy.prototype.leave = function (e) {
+        var base = this;
+
+        if (e.relatedTarget == base.$tip[0]) {
+            base.$tip.bind('mouseleave', function (e) {
+                if (e.relatedTarget == base.$el[0]) {
+                    return;
+                }
+                base.settings.hide(e, base.$tipsy.stop(true, true));
+            });
+            return;
+        }
+        base.settings.hide(e, base.$tipsy.stop(true, true));
     };
 
     $.tooltipsy.prototype.readify = function () {
@@ -131,13 +156,14 @@
         offset: [0, -1],
         content: '',
         show: function (e, $el) {
-            $el.css('opacity', '1').show(100);
+            $el.fadeIn(100);
         },
         hide: function (e, $el) {
             $el.fadeOut(100);
         },
         css: {},
-        className: 'tooltipsy'
+        className: 'tooltipsy',
+        delay: 200
     };
 
     $.fn.tooltipsy = function(options) {
